408c919c7dbd2ac43ede9fdf5ca87579d0e5303f
karendepaz <karendeyanira02@gmail.com>
2022-06-09 11:26:54 -0500

    updated mine class

--- mine.py
+++ mine.py
@@ -1,4 +1,8 @@
 from falling_object import FallingObject
 
-class Mine:
-    pass
+class Mine(FallingObject):
+    
+    def __init__(self):
+        super(Mine, self).__init__()
+        self.appearance = "x"
+        self.points = -20

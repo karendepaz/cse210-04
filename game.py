47b790110bbde6bfb0f7bf05c2ea70807494eab9
Bryce Sanders <104106284+bryce-sanders@users.noreply.github.com>
2022-06-09 09:14:13 -0700

    Program Bones

--- game.py
+++ game.py
@@ -1,26 +1,54 @@
-import pyray as pr
-
-"""
-the game play loop goes here.
-"""
+from pyray import *
+import random
+from player import Player
+from score import Score
+from gem import Gem
+from rock import Rock
+from mine import Mine
 
 class Game:
 
-    #open a window 
-    pr.init_window(800, 450, "Hello Pyray")
-    pr.set_target_fps(60)
+    def __init__(self):
 
-   # camera = pr.Camera3D([18.0, 16.0, 18.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0], 45.0, 0)
-   # pr.set_camera_mode(camera, pr.CAMERA_ORBITAL)
+        self.player = Player()
+        self.falling_objects = []
+        self.score = Score()
+        self.add_object_counter = 0
 
-    while not pr.window_should_close():
-        #pr.update_camera(camera)
-        pr.begin_drawing()
-        pr.clear_background(pr.BLACK)
-        #pr.begin_mode_3d(camera)
-       # pr.draw_grid(20, 1.0)
-        pr.end_mode_3d()
-        pr.draw_text("Hello world", 190, 200, 20, pr.VIOLET)
-        pr.end_drawing()
-    pr.close_window()
+    def start_game(self):
 
+        init_window(900, 600, "Greed")
+
+        while not window_should_close():
+            begin_drawing()
+            clear_background(BLACK)
+            self.check_object_removal()
+            if self.add_object_counter > 500:
+                picker = random.randint(1, 100)
+                if picker in range(0, 6):
+                    object = Mine()
+                elif picker in range(6, 61):
+                    object = Rock()
+                elif picker in range(61, 101):
+                    object = Gem()
+                self.falling_objects.append(object)
+                self.add_object_counter = 0
+            else:
+                self.add_object_counter += 1
+            self.score.display_score()
+            for object in self.falling_objects:
+                object.fall()
+                draw_text(object.appearance, object.position.x, object.position.y, 15, RED)
+            self.player.move()
+            draw_text(self.player.appearance, self.player.position.x, self.player.position.y, 15, WHITE)
+            end_drawing()
+        close_window()
+
+    def check_object_removal(self):
+        for object in self.falling_objects:
+            if object.position.x in range(self.player.position.x - 8, self.player.position.x + 8):
+                if object.position.y in range(self.player.position.y - 8, self.player.position.y + 8):
+                    self.falling_objects.remove(object)
+                    self.score.value += object.points
+            if object.position.y == 590:
+                self.falling_objects.remove(object)
--- player.py
+++ player.py
@@ -1,11 +1,100 @@
-from moving_object import Moving_Object
-"""
-moves the player (#) left and right on the screen.
-inherits from moving_object class
-"""
+import pyray
+from point import Point
 
-class Player(Moving_Object):
-    
+class Player:
+
     def __init__(self):
-        super().__init__()
+        self.appearance = "#"
+        self.position = Point(450, 300)
+        self.move_counter = 0
 
+    def move(self):
+        if self.position.x == 2:
+            self.position.x += 1
+            return
+
+        if self.position.x == 890:
+            self.position.x -= 1
+            return
+
+        if self.position.y == 2:
+            self.position.y += 1
+            return
+
+        if self.position.y == 590:
+            self.position.y -= 1
+            return
+
+        if pyray.is_key_down(pyray.KEY_LEFT) and pyray.is_key_down(pyray.KEY_RIGHT):
+            return
+
+        if pyray.is_key_down(pyray.KEY_LEFT) and pyray.is_key_down(pyray.KEY_UP):
+            if self. move_counter < 20:
+                self.move_counter += 1
+            else:
+                self.position.x -= 1
+                self.position.y -= 1
+                self.move_counter = 0
+            return
+
+        if pyray.is_key_down(pyray.KEY_LEFT) and pyray.is_key_down(pyray.KEY_DOWN):
+            if self. move_counter < 20:
+                self.move_counter += 1
+            else:
+                self.position.x -= 1
+                self.position.y += 1
+                self.move_counter = 0
+            return
+
+        if pyray.is_key_down(pyray.KEY_RIGHT) and pyray.is_key_down(pyray.KEY_UP):
+            if self. move_counter < 20:
+                self.move_counter += 1
+            else:
+                self.position.x += 1
+                self.position.y -= 1
+                self.move_counter = 0
+            return
+
+        if pyray.is_key_down(pyray.KEY_RIGHT) and pyray.is_key_down(pyray.KEY_DOWN):
+            if self. move_counter < 20:
+                self.move_counter += 1
+            else:
+                self.position.x += 1
+                self.position.y += 1
+                self.move_counter = 0
+            return
+
+        if pyray.is_key_down(pyray.KEY_UP) and pyray.is_key_down(pyray.KEY_DOWN):
+            return
+
+        if pyray.is_key_down(pyray.KEY_LEFT):
+            if self. move_counter < 20:
+                self.move_counter += 1
+            else:
+                self.position.x -= 1
+                self.move_counter = 0
+            return
+        
+        if pyray.is_key_down(pyray.KEY_RIGHT):
+            if self. move_counter < 20:
+                self.move_counter += 1
+            else:
+                self.position.x += 1
+                self.move_counter = 0
+            return
+        
+        if pyray.is_key_down(pyray.KEY_UP):
+            if self. move_counter < 20:
+                self.move_counter += 1
+            else:
+                self.position.y -= 1
+                self.move_counter = 0
+            return
+        
+        if pyray.is_key_down(pyray.KEY_DOWN):
+            if self. move_counter < 20:
+                self.move_counter += 1
+            else:
+                self.position.y += 1
+                self.move_counter = 0
+            return
